4f021e83ad24bcd898be04f3a72095f8ca21b93e
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-07-08 16:53:52 +0300

    add 3 columns

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 
-
 const ImageGallery = ({ images }) => {
   return (
-    <div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gap: " 10px",
+      }}
+    >
       {images.map((el, index) => (
-        <img
-          key={index}
-          src={el}
-          style={{ width: "200px", margin: "10px" }}
-        />
+        <img key={index} src={el} style={{ width: "200px", margin: "10px" }} />
       ))}
     </div>
   );
-}
+};
 
-export default ImageGallery
+export default ImageGallery;
