051aec4968cdc286da1d7d774edf71f41526776f
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-07-08 21:26:33 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import MyComp from "./MyComp";
+import DogsPro from "./DogsPro";
 
 function App() {
   return (
     <>
-      <MyComp />
+      <DogsPro />
     </>
   );
 }
