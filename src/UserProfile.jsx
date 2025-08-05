1541c6df1da0e80ea9f025619b333318139bab90
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-08-05 18:03:45 +0300

    moved the object to a separate file

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,19 +1,8 @@
 import { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
-import { ThemeContext } from "./ThemeContext";
+import { translations } from "./translations";
 
 export const UserProfile = () => {
-  const translations = {
-    en: {
-      welcome: "Welcome",
-      profile: "Your profile",
-    },
-    ru: {
-      welcome: "Добро пожаловать",
-      profile: "Твой профиль",
-    },
-  };
-
   const { language } = useContext(LanguageContext);
 
   return <h3>{translations[language].profile}</h3>;
