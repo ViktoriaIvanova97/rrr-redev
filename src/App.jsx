1a5fc8c231d92fe9ad80ec244fe8f4aee4af2bc7
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-09-08 15:43:53 +0300

    start task

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import "./App.css";
-import { Routes, Route, NavLink, Navigate } from "react-router";
-import MainPage from "./MainPage";
-import { Home } from "./Home";
-import { ButtonTheme } from "./ButtonTheme";
-import { ThemeProvider } from "./ThemeProvider"; 
 
 function App() {
-  return (
-    <ThemeProvider>
-      <nav>
-        <ButtonTheme />
-        <NavLink to={"/home"} style={{ color: "#121212" }}>
-         <p>ДОМОЙ</p>
-        </NavLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/mainPage/*" element={<MainPage />} />
-      </Routes>
-    </ThemeProvider>
-  );
+  return <></>;
 }
 
 export default App;
